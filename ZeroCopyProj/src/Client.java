c34d52528db7ead55fb3becae78f60ac03b314be
Natthavut Apinantakun <apinantakun_n@silpakorn.edu>
2021-11-08 10:31:54 +0700

    initial commit

--- ZeroCopyProj/src/Client.java
+++ ZeroCopyProj/src/Client.java
@@ -6,14 +6,14 @@ import java.util.Scanner;
 
 public class Client {
 
-    private Socket clientSocket;
-    private SocketChannel clientChannel;
-    private DataInputStream din;
-    private DataOutputStream dout;
+    private Socket clientSocket; //ตัวระบุยืนยันตัวตน
+    private SocketChannel clientChannel;//ตัวระบุยืนยันตัวตน
+    private DataInputStream din; //รับข้อมูล
+    private DataOutputStream dout; //ส่งข้อมูล
     private final String IP = "172.20.10.8"; //ip standart vm
     private final int PORT_SERVER = 8040;
     private final int PORT_SOCKET = 4070;
-    private final String folder = "C:/Users/api_q/OneDrive/เดสก์ท็อป/ZeroCopyProj/client";
+    private final String folder = "C:/Users/api_q/OneDrive/เดสก์ท็อป/ZeroCopyProj/client"; //ที่เก็บไฟล์เมื่อทำการกาวน์โหลด
     private String[] file;
 
     public static void main(String[] args) {
@@ -23,30 +23,30 @@ public class Client {
 
     private void connectionToServer() {
         try {
-            clientSocket = new Socket(IP, PORT_SERVER);
-            clientChannel = SocketChannel.open(new InetSocketAddress(IP, PORT_SOCKET));
-            din = new DataInputStream(clientSocket.getInputStream());
-            dout = new DataOutputStream(clientSocket.getOutputStream());
+            clientSocket = new Socket(IP, PORT_SERVER); //สิ่งที่ระบุตัวตนของ client
+            clientChannel = SocketChannel.open(new InetSocketAddress(IP, PORT_SOCKET));//สิ่งที่ระบุตัวตนของ client
+            din = new DataInputStream(clientSocket.getInputStream()); //รับข้อมูล
+            dout = new DataOutputStream(clientSocket.getOutputStream()); //ส่งข้อมูล
             Scanner sc = new Scanner(System.in);
 
-            int listFile = din.readInt();
+            int listFile = din.readInt(); //รับขนาดไฟล์
             file = new String[listFile];
             System.out.println("-----------------------File------------------------");
             for (int i = 0; i < listFile; i++) {
                 file[i] = din.readUTF();
                 System.out.println("(" + (i + 1) + ")" + file[i]);
-            }
+            }//รับชื่อไฟลืทั้งหมด
             System.out.println("---------------------------------------------------");
             System.out.println("Which file do you want to download?");
             System.out.print("Press your number : ");
-            int select = sc.nextInt();
-            dout.writeInt(select);
+            int select = sc.nextInt(); //ใส่เลขไฟล์ที่ต้องการ
+            dout.writeInt(select); //ส่งไฟล์ที่ต้องการ
             System.out.println("Which solution do you want to download?");
             System.out.println("(1)" + " Copy");
             System.out.println("(2)" + " ZeroCopy");
             System.out.print("Press your number : ");
             int solution = sc.nextInt();
-            dout.writeInt(solution);
+            dout.writeInt(solution); //ส่งค่าวิธีที่ต้องการโหลดไปหา server
             String path = folder + "/" + file[select - 1];
             long size = din.readLong();
             if (solution == 1) {
@@ -74,7 +74,7 @@ public class Client {
             byte[] buffer = new byte[1024];
             long currentRead = 0;
             while (currentRead < sizeFile) {
-                int read = din.read(buffer);
+                int read = din.read(buffer); //อ่านข้อมูลเข้ามาทีละ 1024
                 currentRead += read;
                 bos.write(buffer, 0, read);
                 System.out.println("Receiving : "+read);
@@ -90,7 +90,7 @@ public class Client {
             FileChannel fileChannel = new FileOutputStream(pathFile).getChannel();
             int currentRead = 0;
             while (currentRead < sizeFile) {
-                long read = fileChannel.transferFrom(clientChannel, currentRead, sizeFile - currentRead);
+                long read = fileChannel.transferFrom(clientChannel, currentRead, sizeFile - currentRead);//อ่านข้อมูลไฟล์ที่ส่งมา
                 currentRead += read;
                 System.out.println("Receiving : "+read);
             }
--- ZeroCopyProj/src/Server.java
+++ ZeroCopyProj/src/Server.java
@@ -5,13 +5,13 @@ import java.net.*;
 
 public class Server {
 
-    private ServerSocket serverSocket;
-    private ServerSocketChannel serverChannel;
+    private ServerSocket serverSocket; //เชื่อม server กับ client ตัวนี้จะเป็นของ copy
+    private ServerSocketChannel serverChannel;//เชื่อม server กับ client ตัวนี้จะเป็นของ ZeroCopy
     private final String IP = "172.20.10.8"; //ip standart vm
-    private final int PORT_SERVER = 8040;
-    private final int PORT_SOCKET = 4070;
-    private int countClient = 0;
-    private final String folder = "C:/Users/api_q/OneDrive/เดสก์ท็อป/ZeroCopyProj/server";
+    private final int PORT_SERVER = 8040; //port ของ Socket
+    private final int PORT_SOCKET = 4070;//port ของ SocketChannel
+    private int countClient = 0; //นับclientที่เชื่อมเข้ามา
+    private final String folder = "C:/Users/api_q/OneDrive/เดสก์ท็อป/ZeroCopyProj/server"; //path ไฟล์ที่ทำหน้าที่เก็บไฟล์ที่ต้องการโหลด
 
     public static void main(String[] args) {
         Server server = new Server();
@@ -22,22 +22,22 @@ public class Server {
     }
 
     private File[] fetchFile() {
-        return new File(folder).listFiles((File file) -> file.isFile()); //judge file?
+        return new File(folder).listFiles((File file) -> file.isFile()); //ตรวจสอบว่าเป็นไฟล์จริงๆไหม?
     }
 
     private void connected() {
         try {
-            serverSocket = new ServerSocket(PORT_SERVER);
-            serverChannel = ServerSocketChannel.open();
+            serverSocket = new ServerSocket(PORT_SERVER); //ทำการเชื่อมโดยใส่ port กำหนดไว้
+            serverChannel = ServerSocketChannel.open();//ทำการเชื่อมโดยใส่ port กำหนดไว้
             serverChannel.bind(new InetSocketAddress(IP, PORT_SOCKET));
             System.out.println("Server waiting for client on port : " + serverSocket.getLocalPort());
             System.out.println("Server waiting ...");
             while (true) {
-                Socket client = serverSocket.accept();
-                SocketChannel clientChannel = serverChannel.accept();
+                Socket client = serverSocket.accept(); //จุดยอมรับการเชื่อมต่อ
+                SocketChannel clientChannel = serverChannel.accept();//จุดยอมรับการเชื่อมต่อ
                 new Thread(new HandleClient(client, clientChannel, ++countClient, fetchFile())).start();
                 System.out.println("New connection accepted : " + client.getInetAddress() + " : " + client.getLocalPort() + " --> Cilent No. [" + countClient + "]");
-            }
+            } //เหตุผลที่เป็น loop while เพราะว่าเราไม่รู้จำนวน client ที่จะเชื่อมต่อ
         } catch (IOException e) {
             System.err.println(e);
         }
@@ -45,12 +45,12 @@ public class Server {
 
     class HandleClient implements Runnable {
 
-        private final Socket clientSocket;
-        private final SocketChannel clientChannel;
-        private DataInputStream din;
-        private DataOutputStream dout;
-        private final int clientNo;
-        private final File[] file;
+        private final Socket clientSocket; //เป็นสิ่งที่ยืนยันตัวตน
+        private final SocketChannel clientChannel;//เป็นสิ่งที่ยืนยันตัวตน
+        private DataInputStream din; //รับ
+        private DataOutputStream dout; //ส่ง
+        private final int clientNo; //ทำหน้าที่จำแนกว่าเป็นคนที่เท่าไหร่
+        private final File[] file; //ทำหน้าที่เก็บลิสต์ไฟล์
 
         public HandleClient(Socket clientSocket, SocketChannel clientChannel, int clientNo, File[] file) {
             this.clientSocket = clientSocket;
@@ -62,18 +62,18 @@ public class Server {
         @Override
         public void run() {
             try {
-                din = new DataInputStream(clientSocket.getInputStream());
-                dout = new DataOutputStream(clientSocket.getOutputStream());
+                din = new DataInputStream(clientSocket.getInputStream()); //รับข้อมูล
+                dout = new DataOutputStream(clientSocket.getOutputStream()); //ส่งข้อมูล
 
-                dout.writeInt(file.length); //ขนาดไฟล์ทั้งหมด
+                dout.writeInt(file.length); //ส่งขนาดไฟล์ทั้งหมดไปหา client
                 for (int i = 0; i < file.length; i++) {
 
-                    dout.writeUTF(file[i].getName());//send name all file
+                    dout.writeUTF(file[i].getName());//ส่งชื่อไฟล์ไปหาclientทั้งหมด
                 }
-                int select = din.readInt();
+                int select = din.readInt(); //อ่านไฟล์ที่ต้องการจาก clinet
                 System.out.println("Cilent No. [" + countClient + "] : Reques File : " + file[select - 1].getName());
-                int solution = din.readInt();
-                dout.writeLong(file[select - 1].length());
+                int solution = din.readInt(); //อ่านวิธีที่ต้องการโหลด
+                dout.writeLong(file[select - 1].length()); //ส่งขนาดไฟล์ทั้งหมดไป
                 if (solution == 1) {
                     System.out.println("Cilent No. [" + countClient + "] : Solution : Copy");
                     int start = (int) System.currentTimeMillis(); 
@@ -99,14 +99,14 @@ public class Server {
 
         private void copy(String pathFile, long sizeFile) {
             try {
-                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(pathFile)); 
-                byte[] buffer = new byte[1024];
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(pathFile));  //เก็บลงที่ buffer
+                byte[] buffer = new byte[1024]; //เขียนทีละ 1024
                 long currentRead = 0;
                 while (currentRead < sizeFile) {
-                    int read = bis.read(buffer);
-                    currentRead += read;
-                    dout.write(buffer, 0, read);
-                    System.out.println("Downloading : "+currentRead);
+                    int read = bis.read(buffer);//อ่านที่ละ 1024
+                    currentRead += read; //ทำการบวกค่าข้อมูลที่อ่านแล้ว
+                    dout.write(buffer, 0, read); //เขียนข้อมูลตั้งแต่เริ่มต้นจนถึง read
+                    System.out.println("Downloading : "+ currentRead + " From : "+ sizeFile );
                 }
                 bis.close();
             } catch (IOException e) {
@@ -119,7 +119,7 @@ public class Server {
                 FileChannel fileChannel = new FileInputStream(pathFile).getChannel();
                 int currentRead = 0;
                 while (currentRead < sizeFile) {
-                    long read = fileChannel.transferTo(currentRead, sizeFile - currentRead, clientChannel);
+                    long read = fileChannel.transferTo(currentRead, sizeFile - currentRead, clientChannel); //ทำการโอนถ่ายข้อมูลไปหา client
                     currentRead += read;
                     System.out.println("Downloading : "+currentRead);
                 }
